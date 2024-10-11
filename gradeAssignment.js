a036c8d6e3e56d2f1eede0e14a94eea614dacdab
unknown <ganijustine8@gmail.com>
2024-10-11 11:52:37 +0800

    initial commit

---
+++ gradeAssignment.js
@@ -0,0 +1,12 @@
+function gradeAssignment() {
+  let grade = prompt("Enter your grade: ");
+  if (grade >= 90) {
+      alert("Excellent");
+  } else if (grade >= 80) {
+      alert("Good");
+  } else if (grade >= 70) {
+      alert("Fair");
+  } else {
+      alert("Needs Improvement");
+  }
+}
